ab1a0cbe902d6892811c9b9b18d6345883c4bc89
Margaret James <mpinaforejames@gmail.com>
2018-06-03 14:06:33 -0600

    cleanup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,22 +5,13 @@ import { FETCH_CITIES, FETCH_CITY } from './types';
 export const fetchAllCitiesReadOnly = (state) => async dispatch => {
   const res = await axios.get(`/api/readonlycitylist`);
   dispatch({ type: FETCH_CITIES, payload: res.data });
-// ADMIN EDIT, formerly 'api/edit/{city}'
-export const fetchCityEdit = (city) => async dispatch => {
-	const res = await axios.get(`/api/adminedit/${city}`);
-	dispatch({ type: FETCH_CITY, payload: res.data });
-};
-// ADMIN EDIT, formerly 'api/edit/{city}'
-export const fetchCityEdit = (city) => async dispatch => {
-	const res = await axios.get(`/api/adminedit/${city}`);
-	dispatch({ type: FETCH_CITY, payload: res.data });
-};
 };
 // USER DASH, formerly 'fetchCity'
 export const fetchCityReadOnly = (city) => async dispatch => {
 	const res = await axios.get(`/api/readonlycity/${city}`);
 	dispatch({ type: FETCH_CITY, payload: res.data });
 };
+
 // ADMIN DASH
 export const fetchAllCitiesAdmin = (state) => async dispatch => {
 	const res = await axios.get(`/api/admincitylist`);
